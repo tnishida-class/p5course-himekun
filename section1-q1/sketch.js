bb975b9932431e693e335b25534451327356941c
miu <miumiu-3535-@outlook.jp>
2021-10-14 21:59:31 +0900

    Change section1-1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -7,8 +7,9 @@ function setup(){
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  //arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
   // BLANK[1]
+  arc(100, 100, 100, 100, QUARTER_PI, QUARTER_PI * 3);
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);

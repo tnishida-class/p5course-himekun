95f3e9d5be0792b30772a02ac7fba22ed3e671dc
miu <miumiu-3535-@outlook.jp>
2021-11-28 00:44:50 +0900

    Change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -5,12 +5,20 @@ let cycle;
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 50;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+ let size = count
+ if(40 < count && count < 60){
+   ellipse(width / 2, height/2, size*2);
+   }
+ else{
+   ellipse(width / 2, height / 2, size);
+ if(mouseIsPressed){
+   count = (count + 5)
+ }
+ }
 }
